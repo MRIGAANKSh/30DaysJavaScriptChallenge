ba0f015bbc57e29ececf3e64b4568b4926bbbac7
MRIGAANKSh <mrigaanksharma928@gmail.com>
2024-07-13 13:41:25 +0530

    first day

---
+++ Day1.js
@@ -0,0 +1,38 @@
+//Activity 1//
+var a=24;
+let b=2005;
+console.log(a);
+console.log(b)
+
+
+//Activity 2//
+
+
+const c=true;
+console.log(c);
+
+//Activty3 //
+
+let d=123;
+let e="mrigaank sharma";
+const f=[24,2,2005,"mrigaank"];
+let g={
+    name:"mrigaank",
+    age:19,
+    dob:24
+}
+console.log(typeof(d),typeof(e),typeof(f),typeof(g));
+
+//Activty4//
+
+let h=24;
+console.log(h,"intial");
+h=2005;
+console.log(h,"final");
+
+//Activty5//
+const i=2402;
+i=24;
+console.log(i);
+
+
