76ca5b631a7732825d46b69a91a2927909af9f9b
MRIGAANKSh <mrigaanksharma928@gmail.com>
2024-07-14 13:01:09 +0530

    second Day

---
+++ Day2.js
@@ -0,0 +1,47 @@
+//Activty1
+
+let a=20;
+let b=24;
+console.log(a+b);//add
+console.log(a-b);//subtract
+console.log(a*b);//multiply
+console.log(a/b);//divide
+console.log(b%a);//remainder
+
+//Activty 2
+
+a+=2;
+console.log(a)
+b-=4;
+console.log(b)
+
+//Activty 3
+
+console.log(a>b,a,b);
+console.log(a<b,a,b);
+
+let c=22;
+let d=22;
+let e=244;
+console.log(c>=d);
+console.log(d<=e);
+
+let f="24";
+let g=24;
+console.log("==",f==g);
+console.log("===",f===g);
+
+//Activty 4
+
+let m=24;
+let k=25;
+console.log(m>20&&k<30);
+console.log(m>20||k>30,k);
+console.log(m!=23);
+
+//Activty 5
+
+m=(m>0)?"+ve":"-ve";
+console.log(m)
+
+
